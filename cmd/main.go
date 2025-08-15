621e19d66b678357e5022ea3b078808ce53973fe
Done Jovi <donejovi@gmail.com>
2025-08-15 19:49:48 +0700

    Add transaction handling

--- cmd/main.go
+++ cmd/main.go
@@ -17,12 +17,15 @@ func main() {
 
 	// Inisialisasi repository
 	userRepo := repository.NewUserRepositoryImpl(db)
+	transactionRepo := repository.NewTransactionRepositoryImpl(db)
 
 	// Inisialisasi service
 	userService := services.NewUserService(userRepo)
+	transactionService := services.NewTransactionService(transactionRepo, db)
 
 	// Inisialisasi handler
 	userHandler := http.NewUserHandler(*userService)
+	transactionHandler := http.NewTransactionHandler(*transactionService)
 
 	// Setup router menggunakan Gin
 	r := gin.Default()
@@ -30,6 +33,10 @@ func main() {
 	// Endpoint user
 	r.POST("/register", userHandler.Register)
 	r.POST("/login", userHandler.Login)
+
+	// Endpoint transaction
+	r.POST("/deposit", transactionHandler.Deposit)
+	r.POST("/withdraw", transactionHandler.Withdraw)
 
 	// Jalankan server pada port 8080
 	r.Run(":8080")
